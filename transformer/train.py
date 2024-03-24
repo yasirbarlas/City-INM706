d6a5a2df0eca4c85820ddb89fd612d1280a03295
Yasir Barlas <79994089+yasirbarlas@users.noreply.github.com>
2024-03-24 20:20:21 +0000

    Update train.py

--- transformer/train.py
+++ transformer/train.py
@@ -15,13 +15,9 @@ import matplotlib.ticker as ticker
 from torch.utils.data import Dataset, DataLoader, random_split
 from utils import parse_arguments, read_settings, calculate_bleu, calculate_nist
 
-
-device = torch.device('cpu')
+device = torch.device("cpu")
 if torch.cuda.is_available():
-    device = torch.device('cuda')
-
-wandb_logger = Logger(f"inm706_translation_transformer", project='inm706_cwkk')
-logger = wandb_logger.get_logger()
+    device = torch.device("cuda")
 
 def train_epoch(train_dataloader, model, optimizer, criterion, use_gradient_clipping, plot_attention = False):
     model.train()
@@ -75,18 +71,17 @@ def validate_epoch(val_dataloader, model, criterion):
 
     return total_loss / len(val_dataloader), total_bleu_score / len(val_dataloader), total_nist_score / len(val_dataloader)
 
-def train(train_dataloader, val_dataloader, model, n_epochs, criterion, use_gradient_clipping, learning_rate=0.0001, optimizer="adam" ):
+def train(train_dataloader, val_dataloader, model, n_epochs, criterion, use_gradient_clipping, learning_rate = 0.0001, optimizer = "adam" ):
     val_losses = [float("inf")]
     counter = 0
-
+    
     if optimizer == "adam":
-        optimizer = optim.Adam(model.parameters(), lr = learning_rate, betas=(0.9, 0.98), eps=1e-9)
+        optimizer = optim.Adam(model.parameters(), lr = learning_rate, betas=(0.9, 0.98), eps = 1e-9)
     elif optimizer == "radam":
         optimizer = optim.RAdam(model.parameters(), lr = learning_rate)
     elif optimizer == "adamW":
-        optimizer = optim.AdamW(model.parameters(), lr = learning_rate, weight_decay=1e-5)
+        optimizer = optim.AdamW(model.parameters(), lr = learning_rate, weight_decay = 1e-5)
 
-    
     if criterion == "negative-log":
         criterion = nn.NLLLoss()
     elif criterion == "cross-entropy":
@@ -98,13 +93,13 @@ def train(train_dataloader, val_dataloader, model, n_epochs, criterion, use_grad
 
         val_losses.append(val_loss)
 
-        logger.log({'train_loss': train_loss, 'val_loss': val_loss})
-        logger.log({"train_bleu": train_bleu, "validation_bleu": val_bleu})
-        logger.log({"train_nist": train_nist, "validation_nist": val_nist})
+        logger.log({"train_loss": train_loss, "val_loss": val_loss})
+        logger.log({"train_bleu": train_bleu, "val_bleu": val_bleu})
+        logger.log({"train_nist": train_nist, "val_nist": val_nist})
         print(f"Epoch: {epoch} / {n_epochs}, Train Loss {train_loss}, Validation Loss {val_loss}, Train BLEU {train_bleu}, Validation BLEU {val_bleu}, Train NIST {train_nist}, Validation NIST {val_nist}")
         #print(f'Epoch [{epoch + 1}/{n_epochs}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
 
-        if (val_loss + 0.01) < val_losses[epoch - 1]:
+        if (val_loss + 0.001) < val_losses[epoch - 1]:
             # Restart patience (improvement in validation loss)
             counter = 0
 
@@ -117,6 +112,8 @@ def train(train_dataloader, val_dataloader, model, n_epochs, criterion, use_grad
             checkpoint_path = os.path.join("checkpoints", checkpoint_name)
             torch.save({
                 "epoch": epoch,
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
                 "train_loss": train_loss,
                 "val_loss": val_loss,
                 "train_bleu": train_bleu,
@@ -125,14 +122,34 @@ def train(train_dataloader, val_dataloader, model, n_epochs, criterion, use_grad
                 "val_nist": val_nist,
             }, checkpoint_path)
         
-        elif (val_loss + 0.01) > val_losses[epoch - 1]:
+        elif (val_loss + 0.001) > val_losses[epoch - 1]:
             # Add one to patience
             counter += 1
+
+            if val_loss < val_losses[epoch - 1]:
+                # Create checkpoint folder
+                if not os.path.exists("checkpoints"):
+                    os.makedirs("checkpoints")
+
+                # Save the model checkpoint
+                checkpoint_name = f"checkpoint_latest.pth"
+                checkpoint_path = os.path.join("checkpoints", checkpoint_name)
+                torch.save({
+                    "epoch": epoch,
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "train_loss": train_loss,
+                    "val_loss": val_loss,
+                    "train_bleu": train_bleu,
+                    "val_bleu": val_bleu,
+                    "train_nist": train_nist,
+                    "val_nist": val_nist,
+                }, checkpoint_path)
+            
             # Patience reached, stop training (no significant improvement in validation loss after 5 epochs)
             if counter >= 5:
                 break
     return
-
 
 def main():
     # Set random seed for reproducibility
@@ -151,19 +168,26 @@ def main():
     # Access and use the settings as needed
     model_settings = settings.get('model', {})
     train_settings = settings.get('train', {})
+    print(model_settings)
+    print(train_settings)
 
+    # Initialise 'wandb' for logging
+    wandb_logger = Logger(f"inm706_transformer", project = "inm706_cwk")
+    logger = wandb_logger.get_logger()
+    
     # Load the dataset and create DataLoader
-    dataset = TranslationDataset(lang1='en', lang2='fr', reverse=True, max_seq_len=model_settings["max_seq_length"])
-    batch_size = train_settings["batch_size"]
-    n_epochs = train_settings["epochs"]
+    dataset = TranslationDataset(lang1 = "en", lang2 = "fr", max_seq_len = model_settings["max_seq_length"], reverse = False)
 
-    # Split dataset into training and validation sets
-    train_size = int(0.75 * len(dataset))
+    # Define the sizes for training and validation sets
+    train_size = int(0.8 * len(dataset))
     val_size = len(dataset) - train_size
+    
+    # Split the dataset into training and validation sets
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size], generator = generator)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
+    # Create data loaders for each set
+    train_dataloader = DataLoader(train_dataset, batch_size=train_settings["batch_size"])
+    val_dataloader = DataLoader(val_dataset, batch_size=train_settings["batch_size"])
 
     # Create the Transformer model with encoder and decoder
     src_vocab_size = dataset.input_lang.n_words
@@ -185,8 +209,7 @@ def main():
     use_gradient_clipping = True
 
     # Training loop
-    train(train_dataloader, val_dataloader, model, n_epochs,  optimizer = train_settings["optimizer"], criterion = train_settings["loss_function"], use_gradient_clipping=True)
+    train(train_dataloader, val_dataloader, model, n_epochs = train_settings["epochs"], optimizer = train_settings["optimizer"], criterion = train_settings["loss_function"], use_gradient_clipping = True)
 
 if __name__ == '__main__':
     main()
-
